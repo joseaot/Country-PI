8d592cc87e601a5c1a945f4adb478a540f0a4884
joseaot <josealbertoaot@gmail.com>
2023-06-29 02:23:09 -0300

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+// const PORT = 3001;
+const {PORT} = process.env
 const { LoadDB } = require('./loadDB/LoadDB')
 
 
 conn.sync({ force: false }).then(() => {
 server.listen(PORT, async() => {
   LoadDB()
-  console.log(`Server listening on port ${PORT}`);
+  console.log('Server listening on port', PORT);
 })
 }).catch(error => console.error(error))
